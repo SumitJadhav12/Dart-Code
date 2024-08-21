122592891bb03170b18bf425f61d9c2651cbb944
SumitJadhav12 <159809606+SumitJadhav12@users.noreply.github.com>
2024-08-21 23:32:54 +0530

    Add files via upload

---
+++ ASSIGNMENT2/program10.dart
@@ -0,0 +1,9 @@
+void main() {
+  int percentage = 80;
+  int cgpa = 8;
+  if (percentage <= 70 || cgpa >= 9) {
+    print("you are  not elgible");
+  } else if (percentage >= 80 || cgpa >= 7.7) {
+    print("You are elgible ");
+  }
+}
---
+++ ASSIGNMENT2/program4.dart
@@ -0,0 +1,8 @@
+void main() {
+  int num = 14;
+  if (num >= 16 & num % 2 == 0) {
+    print("$num Correct number");
+  } else {
+    print("$num Incorrect number");
+  }
+}
